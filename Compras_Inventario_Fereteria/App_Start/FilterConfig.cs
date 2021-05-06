4cab53b49a10d1dc2d8fd636e0c61ae8c86777b8
fher <alex8196@hotmail.es>
2021-05-06 09:43:41 -0600

    Agregar archivos de proyecto.

---
+++ Compras_Inventario_Fereteria/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Compras_Inventario_Fereteria
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Compras_Inventario_Fereteria/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Compras_Inventario_Fereteria.MvcApplication" Language="C#" %>
